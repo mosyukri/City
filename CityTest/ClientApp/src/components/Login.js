21b84f1513688cd4a8ac1e1e0aa7e8953b1dfa3a
mosyukri <68844132+mosyukri@users.noreply.github.com>
2023-06-05 14:12:10 +0800

    Add project files.

---
+++ CityTest/ClientApp/src/components/Login.js
@@ -0,0 +1,40 @@
+﻿import React, { Component } from 'react';
+
+export class Login extends Component {
+    static displayName = Login.name;
+
+    state = {
+        custName: ''
+    };
+
+    handleInputClicked = event => {
+        event.preventDefault();
+
+ /*       helper.onTouchFieldScroll(event.target, 120);*/
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Login</h1>
+                <div style={{ alignContent: 'center' }}>
+                    <label>Customer Name:</label>
+                    <input type="text"
+                        name="custName"
+                        id="custName"
+                        defaultValue={this.state.custName}
+                        onClick={this.handleInputClicked}
+
+                        e
+                    ></input>
+                    <br/>
+                    <br />
+
+                    <input type="button" value="Submit"/>
+                </div>
+
+            </div>
+            
+            );
+    }
+}
---
+++ CityTest/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace CityTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            //retrieve api
+            string urls = "http://alltheclouds.com.au/api/Products";
+
+
+            return Ok(urls);
+        }
+    }
+}
